9bdf4e0980fc2d5a6693a85ac6742187c65624ee
Santhiya natarajan <santhiyanatarajan01@gmail.com>
2025-07-27 21:16:22 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-<<<<<<< HEAD
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-=======
-import './index.css';
-=======
->>>>>>> fcae520 (Initial commit)
-import App from './App';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-<<<<<<< HEAD
 root.render(
   <React.StrictMode>
     <App />
@@ -25,7 +13,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
->>>>>>> 3d27a8a (Initialize project using Create React App)
-=======
-root.render(<App />);
->>>>>>> fcae520 (Initial commit)
+
